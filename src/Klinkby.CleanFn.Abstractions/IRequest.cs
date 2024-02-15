aad40ffd141f1a34c6ff03c40c96334e3acf3aa7
Mads Klinkby <m@kli.dk>
2024-02-15 17:37:06 +0100

    Add Abstractions

---
+++ src/Klinkby.CleanFn.Abstractions/IRequest.cs
@@ -0,0 +1,18 @@
+﻿namespace Klinkby.CleanFn.Abstractions;
+
+/// <summary>
+///     Base interface for request parameters.
+/// </summary>
+public interface IRequestBase;
+
+/// <summary>
+///     Define a command request that does not return a value.
+/// </summary>
+public interface IRequest : IRequestBase;
+
+/// <summary>
+///     Define a query request that returns a response value.
+/// </summary>
+/// <typeparam name="TResponse"></typeparam>
+// ReSharper disable once UnusedTypeParameter
+public interface IRequest<out TResponse> : IRequestBase;
---
+++ src/Klinkby.CleanFn.Abstractions/IRequestHandler.cs
@@ -0,0 +1,44 @@
+﻿namespace Klinkby.CleanFn.Abstractions;
+
+/// <summary>
+///     Base interface for request parameters.
+/// </summary>
+/// <remarks>
+///     Should not be implemented directly,
+///     use <see cref="IRequest{TResponse}" />
+///     or <see cref="IRequest" /> instead.
+/// </remarks>
+public interface IRequestHandlerBase;
+
+/// <summary>
+///     Handler for command requests that does not return a value.
+/// </summary>
+/// <typeparam name="TRequest">The command parameters type.</typeparam>
+public interface IRequestHandler<in TRequest> : IRequestHandlerBase
+    where TRequest : IRequest
+{
+    /// <summary>
+    ///     Handle the command request that does not return a value.
+    /// </summary>
+    /// <param name="command">The command parameters.</param>
+    /// <param name="cancellationToken">A <see cref="CancellationToken" /> used to cancel the operation.</param>
+    /// <returns>An awaitable <see cref="Task" /></returns>
+    Task Handle(TRequest command, CancellationToken cancellationToken);
+}
+
+/// <summary>
+///     Handler for query requests that returns a value.
+/// </summary>
+/// <typeparam name="TRequest">Command parameters type</typeparam>
+/// <typeparam name="TResponse">Response value type</typeparam>
+public interface IRequestHandler<in TRequest, TResponse> : IRequestHandlerBase
+    where TRequest : IRequest<TResponse>
+{
+    /// <summary>
+    ///     Handle the query request that return a value.
+    /// </summary>
+    /// <param name="query">The query parameters.</param>
+    /// <param name="cancellationToken">A <see cref="CancellationToken" /> used to cancel the operation.</param>
+    /// <returns>An awaitable <see cref="Task{TResponse}" /> with the response value.</returns>
+    Task<TResponse> Handle(TRequest query, CancellationToken cancellationToken);
+}
